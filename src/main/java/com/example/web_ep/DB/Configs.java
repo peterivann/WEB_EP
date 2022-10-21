32b59f247c83e02d58cbaa9ea557124b0987563c
Ivanov Peter <peterivan5555@gmail.com>
2022-10-22 02:43:59 +0700

    Initial commit

---
+++ src/main/java/com/example/web_ep/DB/Configs.java
@@ -0,0 +1,9 @@
+package com.example.web_ep.DB;
+
+public class Configs {
+    protected static String dbHost = "localhost";
+    protected static String dbPort = "3306";
+    protected static String dbUser = "root";
+    protected static String dbPass = "1234";
+    protected static String dbName = "javapr";
+}
---
+++ src/main/java/com/example/web_ep/Server/Config.java
@@ -0,0 +1,8 @@
+package com.example.web_ep.Server;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class Config extends Application {
+}
